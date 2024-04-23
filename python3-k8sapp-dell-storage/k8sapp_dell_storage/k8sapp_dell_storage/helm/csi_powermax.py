80ca83126070bd7fad4afa759a225d5f616bef03
Gustavo Ornaghi Antunes <gustavo.ornaghiantunes@windriver.com>
2024-04-23 07:54:32 -0300

    csi-powermax: Include CSI-PowerMax Helm Chart
    Including the CSI-PowerMax helm chart to App Dell Storage repository.
    
    supported_k8s_version: ">= 1.23.0 < 1.30.0"
    
    Test Plan:
      PASS: All packages built
      PASS: Upload/apply/remove/delete/update dell-storage app in
            AIO-SX VM.
      PASS: Enable csm-replication and csm-authorization charts and apply
            overrides.
      PASS: Check if the replication and authorization pods are running.
      PASS: Check if enabled sidecar containers are running.
    
    Story: 2010693
    Task: 49920
    
    Change-Id: I6309d1bd085114bdb5a2b729863aeeb27af31784
    Signed-off-by: Gustavo Ornaghi Antunes <gustavo.ornaghiantunes@windriver.com>

---
+++ python3-k8sapp-dell-storage/k8sapp_dell_storage/k8sapp_dell_storage/helm/csi_powermax.py
@@ -0,0 +1,38 @@
+#
+# Copyright (c) 2024 Wind River Systems, Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from sysinv.common import exception
+
+from k8sapp_dell_storage.common import constants as app_constants
+from k8sapp_dell_storage.helm import storage
+
+
+class CSIPowerMaxHelm(storage.StorageBaseHelm):
+    """ Class to encapsulate helm operations for the CSI PowerMax chart. """
+
+    CHART = app_constants.HELM_CHART_CSI_POWERMAX
+    HELM_RELEASE = app_constants.FLUXCD_HELMRELEASE_CSI_POWERMAX
+    SERVICE_NAME = app_constants.HELM_APP_DELL_STORAGE
+
+    def get_overrides(self, namespace=None):
+
+        controller = {
+            "controllerCount": self._num_replicas_for_platform_app()
+        }
+
+        overrides = {
+            app_constants.HELM_NS_DELL_STORAGE: {
+                "controller": controller,
+            }
+        }
+
+        if namespace in self.SUPPORTED_NAMESPACES:
+            return overrides[namespace]
+        elif namespace:
+            raise exception.InvalidHelmNamespace(chart=self.CHART,
+                                                 namespace=namespace)
+        else:
+            return overrides
